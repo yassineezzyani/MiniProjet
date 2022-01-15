20970d66f98e3507d78e7ad4a973c73fcfc52aef
yassineezziyani <yassineezziyani20@gmail.com>
2022-01-15 17:25:03 +0000

    espace

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
         <label for="Nom">Nom</label><br>
         <input type="text" name="Nom" required><br>
         <label for="email">E-mail</label><br>
-        <input type="email" name="email" required><br><br>
+        <input type="email" name="email" required><br><br><br>
         <input type="submit" value="Envoyer">
+
 
     </form>
 
